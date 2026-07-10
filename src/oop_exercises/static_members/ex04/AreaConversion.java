88c5f734a40f3ea189e7c1e2d8a9f89ac59cc0f0
Wesley <palletral3z@gmail.com>
2026-07-10 15:45:21 -0300

    Exercise 04 done.

--- src/oop_exercises/static_members/ex04/AreaConversion.java
+++ src/oop_exercises/static_members/ex04/AreaConversion.java
@@ -1,19 +1,19 @@
-package oop_exercises.static_members.ex03;
+package oop_exercises.static_members.ex04;
 
-class AreaConversion {
+public class AreaConversion {
     public static double squareMetersToSquareFeet(double squareMeters) {
-        return squareMeters*10.76;
+        return squareMeters * 10.76;
     }
 
     public static double squareFeetToSquareCentimeters(double squareFeet) {
-        return squareFeet*929;
+        return squareFeet * 929;
     }
 
     public static double squareMilesToAcres(double squareMiles) {
-        return squareMiles*640;
+        return squareMiles * 640;
     }
 
     public static double acreToSquareFeet(double acre) {
-        return acre*43560;
+        return acre * 43560;
     }
 }
--- src/oop_exercises/static_members/ex04/TestAreaConversion.java
+++ src/oop_exercises/static_members/ex04/TestAreaConversion.java
@@ -1,11 +1,14 @@
-package oop_exercises.static_members.ex03;
+package oop_exercises.static_members.ex04;
 
 public class TestAreaConversion {
     public static void main(String[] args) {
-        System.out.println(AreaConversion.squareMetersToSquareFeet(14));
-        System.out.println(AreaConversion.squareFeetToSquareCentimeters(3));
-        System.out.println(AreaConversion.squareMilesToAcres(10));
-        System.out.println(AreaConversion.acreToSquareFeet(5));
 
+        System.out.println(AreaConversion.squareMetersToSquareFeet(14));
+
+        System.out.println(AreaConversion.squareFeetToSquareCentimeters(3));
+
+        System.out.println(AreaConversion.squareMilesToAcres(10));
+
+        System.out.println(AreaConversion.acreToSquareFeet(5));
     }
 }
