10430ceef897aada53ea9dcd7e876613430c6c28
Wesley <palletral3z@gmail.com>
2026-07-10 14:59:59 -0300

    Exercise 04 done.

---
+++ src/oop_exercises/static_members/ex04/TestAreaConversion.java
@@ -0,0 +1,11 @@
+package oop_exercises.static_members.ex03;
+
+public class TestAreaConversion {
+    public static void main(String[] args) {
+        System.out.println(AreaConversion.squareMetersToSquareFeet(14));
+        System.out.println(AreaConversion.squareFeetToSquareCentimeters(3));
+        System.out.println(AreaConversion.squareMilesToAcres(10));
+        System.out.println(AreaConversion.acreToSquareFeet(5));
+
+    }
+}
